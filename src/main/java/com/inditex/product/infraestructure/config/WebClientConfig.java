22ab8f44696256696cba6fd2dddc791d6ce3ba7e
javier <javiercuervas@hotmail.com>
2025-12-27 00:18:57 +0100

    feat(infrastructure): add web client config

---
+++ src/main/java/com/inditex/product/infraestructure/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.inditex.product.infraestructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder(@Value("${external.api.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl);
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+}
