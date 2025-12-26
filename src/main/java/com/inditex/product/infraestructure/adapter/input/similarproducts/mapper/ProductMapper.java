55d57b0b996e761ef9fd781c2c3f2dde25098391
javier <javiercuervas@hotmail.com>
2025-12-27 00:17:36 +0100

    feat(infrastructure): add input adapters controller, mapper and exception handler

---
+++ src/main/java/com/inditex/product/infraestructure/adapter/input/similarproducts/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.inditex.product.infraestructure.adapter.input.similarproducts.mapper;
+
+import com.inditex.product.api.generated.model.ProductDetail;
+import com.inditex.product.domain.model.Product;
+
+public final class ProductMapper {
+
+    public static ProductDetail toApiModel(Product product) {
+        return new ProductDetail(
+                product.id(),
+                product.name(),
+                product.price(),
+                product.availability()
+        );
+    }
+}
---
+++ src/main/java/com/inditex/product/infraestructure/adapter/input/similarproducts/rest/SimilarProductsController.java
@@ -0,0 +1,33 @@
+package com.inditex.product.infraestructure.adapter.input.similarproducts.rest;
+
+import com.inditex.product.api.generated.ProductApi;
+import com.inditex.product.api.generated.model.ProductDetail;
+import com.inditex.product.application.similar.port.input.GetSimilarProductsUseCase;
+import com.inditex.product.infraestructure.adapter.input.similarproducts.mapper.ProductMapper;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SimilarProductsController implements ProductApi {
+
+    private final GetSimilarProductsUseCase useCase;
+
+    public SimilarProductsController(GetSimilarProductsUseCase useCase) {
+        this.useCase = useCase;
+    }
+
+    @Override
+    public Mono<@NonNull ResponseEntity<@NonNull Flux<@NonNull ProductDetail>>> getProductSimilar(
+            String productId,
+            ServerWebExchange exchange) {
+        return useCase.getSimilarProducts(productId)
+                .map(ProductMapper::toApiModel)
+                .collectList()
+                .map(list -> ResponseEntity.ok(Flux.fromIterable(list)));
+    }
+
+}
